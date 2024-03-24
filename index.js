58d5f38c4eaf3b930cee73baa87aa5ddf3e2bf18
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-03-25 03:03:16 +0500

    added console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("Welcome to my number guessing game");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
